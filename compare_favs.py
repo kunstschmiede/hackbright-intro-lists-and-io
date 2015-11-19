12c12807c78b75fb6576d7fa9bd3223009c54d6c
kunstschmiede <hsburgess@gmail.com>
2015-11-19 14:14:40 -0800

    adding the comparison file

---
+++ compare_favs.py
@@ -0,0 +1,8 @@
+compare_favs = open("hilary_fav_foods.txt")
+hilary_favs = hilary_fav_foods.readlines()
+compare_favs.close()
+
+compare_favs = open("tonya_fav_foods.txt")
+tonya_favs = tonya_fav_foods.readlines()
+compare_favs.close ()
+
