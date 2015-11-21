37f04f8f58167879b08f5de83805a208a7b02b05
kunstschmiede <hsburgess@gmail.com>
2015-11-21 13:34:33 -0800

    complete with second function

--- compare_favs.py
+++ compare_favs.py
@@ -1,13 +1,13 @@
 
 # reading hilary's favorite foods file into a list
 hilary_file = open("hilary_fav_foods.txt")
-hilary_favs = hilary_file.readlines()
+hilary_favs = hilary_file.read().splitlines()
 hilary_file.close()
 
 
 #reading tonya's favorite foods file into a list
 tonya_file = open("tonya_fav_foods.txt")
-tonya_favs = tonya_file.readlines()
+tonya_favs = tonya_file.read().splitlines()
 tonya_file.close()
 
 def compare_favs(a, b):
@@ -17,3 +17,12 @@ def compare_favs(a, b):
 		print "Our favorite foods are different"
 
 compare_favs(hilary_favs, tonya_favs)
+
+def compare_favs2(a, b):
+	for x in a:
+		for y in b:
+			if x == y:
+				print "We both love " + x + "!"
+
+compare_favs2(hilary_favs, tonya_favs)
+
