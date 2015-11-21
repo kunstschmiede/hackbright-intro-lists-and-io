bb2d4354487f52d09869e6824ecbebb45e3a6f14
kunstschmiede <hsburgess@gmail.com>
2015-11-21 13:08:50 -0800

    favorite foods compared

--- compare_favs.py
+++ compare_favs.py
@@ -1,8 +1,19 @@
-compare_favs = open("hilary_fav_foods.txt")
-hilary_favs = hilary_fav_foods.readlines()
-compare_favs.close()
 
-compare_favs = open("tonya_fav_foods.txt")
-tonya_favs = tonya_fav_foods.readlines()
-compare_favs.close ()
+# reading hilary's favorite foods file into a list
+hilary_file = open("hilary_fav_foods.txt")
+hilary_favs = hilary_file.readlines()
+hilary_file.close()
 
+
+#reading tonya's favorite foods file into a list
+tonya_file = open("tonya_fav_foods.txt")
+tonya_favs = tonya_file.readlines()
+tonya_file.close()
+
+def compare_favs(a, b):
+	if (a[0] == b[0]):
+		print "Our favorite foods are the same!"
+	else:
+		print "Our favorite foods are different"
+
+compare_favs(hilary_favs, tonya_favs)
